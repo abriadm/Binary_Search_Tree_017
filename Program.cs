f4c51862591707408c96f84a3622b4d2f6aa0a6f
Giveonaldo <giveonaldo@gmail.com>
2022-12-27 09:14:15 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Binary_Search_Tree
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
