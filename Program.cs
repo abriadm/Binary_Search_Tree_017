04d19f11fe1abb6329bc1780b1cc17e3b9180f47
Giveonaldo <giveonaldo@gmail.com>
2022-12-27 09:15:27 +0700

    create class Node

--- Program.cs
+++ Program.cs
@@ -1,6 +1,15 @@
 ﻿namespace Binary_Search_Tree
 {
+    class Node
+    {
+        public string info;
+        public Node lchild;
+        public Node rchild;
+        public Node()
+        {
 
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
